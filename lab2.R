0d9a5083fbba79f9cc71539effdceee36372b4fb
reed magleby <rem9072@med.cornell.edu>
2018-09-21 22:16:28 -0400

    Made more tidy

--- lab2.R
+++ lab2.R
@@ -4,7 +4,7 @@ library(tidyverse)
 
 lab2 <- read.csv("C:/Users/rfmag/Downloads/lab2.csv")
 lab2mod<-lab2 %>%
-  gather("base_sport","base_qol","base_pain","first_sport","first_qol","first_pain","second_sport", "second_qol", "second_pain", key="time_category", value="score")%>% 
+  gather(2:10, key="time_category", value="score")%>% 
   separate("time_category", c("time","category"), sep= "_") 
 
 #Recreate Graph
@@ -13,7 +13,7 @@ lab2mod %>%
   group_by(category,time)%>%
   summarize(avg=mean(score),stdev=sd(score)) %>%
   ggplot(mapping=aes(x=time, y=avg, group=category, color=category)) + 
-    geom_line(position=position_dodge(0.1)) + 
+  geom_line(position=position_dodge(0.1)) + 
   geom_point(position=position_dodge(0.1)) +
   geom_errorbar(aes(ymin=avg-stdev,ymax=avg+stdev),width=0.05,position=position_dodge(0.1)) +
   labs(x ="Time of report",y ="Sample mean and standard deviation") +
@@ -23,22 +23,25 @@ lab2mod %>%
 
 #Tidy Data part 2
 
-coverage <- read.csv("C:/Users/rfmag/Downloads/coverage.csv",skip=2)
-expend <- read.csv("C:/Users/rfmag/Downloads/expenditures.csv",skip=2)
+coverage <- read.csv("C:/Users/rfmag/Downloads/coverage.csv",skip=2,stringsAsFactors=FALSE,na.strings="N/A",header=TRUE)
+expend <- read.csv("C:/Users/rfmag/Downloads/expenditures.csv",skip=2,stringsAsFactors=FALSE,na.strings="N/A",header=TRUE)
 
 coveragemod<-coverage[-c(68:53),] %>%
-  gather(2:29, key="year_coverage", value="number")%>% 
+  gather(2:29, key="year_coverage", value="number_enrollees")%>% 
   separate("year_coverage", c("year","coverage_type"), sep= "__")
 
 expendmod <- expend[-c(61:53),]%>%
-  select(matches("location|2013|2014")) %>%
+  select(matches("Location|2013|2014")) %>%
   gather(2:3, key="year", value="cost") %>% 
   separate("year", c("year","total health spending"), sep= "__") %>%
   select(one_of(c("Location","year","cost")))
 
 coveragetotal<-filter(coveragemod,coverage_type == "Total") 
 expend_coverage_total<- full_join(coveragetotal,expendmod)
+
 expend_coverage_all <- full_join(expend_coverage_total,coveragemod) %>%
   separate("year", c("x","year"), sep= "X") %>%
   select(-x)
         
+
+
